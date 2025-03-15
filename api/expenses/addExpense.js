75fa0e0c7dbbc5256a53f42a553fbfd98c7ad7d5
riya <riya.b@vrinsoft.com>
2025-03-15 22:01:55 +0530

    basic setup

--- api/expenses/addExpense.js
+++ api/expenses/addExpense.js
@@ -4,17 +4,28 @@ const connectDB = require("../../config/db");
 
 module.exports = async (req, res) => {
   await connectDB();
-  
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
   try {
     const { description, amount, category, recurring, dueDate } = req.body;
-    const newExpense = new Expense({ description, amount, category, recurring, dueDate });
+    console.log(req.body);
+    const newExpense = new Expense({
+      description,
+      amount,
+      category,
+      recurring,
+      dueDate,
+    });
     await newExpense.save();
-    return res.status(201).json({ message: "Expense added successfully", newExpense });
+    return res
+      .status(201)
+      .json({ message: "Expense added successfully", newExpense });
   } catch (error) {
-    return res.status(500).json({ error: "Server Error", details: error.message });
+    return res
+      .status(500)
+      .json({ error: "Server Error", details: error.message });
   }
 };
